91b8295d603484f5983646df6c1875cb52659f24
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2021-06-27 23:58:49 +0900

    Write most of ch4-1

--- assets/js/load-mathjax.js
+++ assets/js/load-mathjax.js
@@ -10,3 +10,10 @@ window.MathJax = {
     mtextInheritFont: true
   },
 };
+
+(function () {
+  var script = document.createElement('script');
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  script.async = true;
+  document.head.appendChild(script);
+})();
