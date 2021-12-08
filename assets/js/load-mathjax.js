aa46063241be1087e7a91a314fbdc0477f524c0b
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2021-12-08 20:54:37 +0900

    Write interference dependence analysis

--- assets/js/load-mathjax.js
+++ assets/js/load-mathjax.js
@@ -1,21 +1,23 @@
-window.MathJax = {
-  tex: {
-    macros: {
-      bm: ["{\\boldsymbol{#1}}",1],
-      inlineMath: [['$', '$'], ['\\(', '\\)']],
-      processEscape: true // use \$ to produce dollar mark
-    }
-  },
-  chtml: {
-    mtextInheritFont: true,
-    //displayAlign: 'left',
-    //displayIndent: '2em'
-  },
-};
+//window.MathJax = {
+//  tex: {
+//    macros: {
+//      bm: ["{\\boldsymbol{#1}}",1],
+//      //inlineMath: [['$', '$'], ['\\(', '\\)']],
+//      processEscape: true, // use \$ to produce dollar mark
+//      processEnvironments: true
+//    }
+//  },
+//  chtml: {
+//    mtextInheritFont: true,
+//    displayAlign: 'left',
+//    displayIndent: '2em'
+//  },
+//};
 
 (function () {
   var script = document.createElement('script');
-  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  //script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/tex-chtml.js";
   script.async = true;
   document.head.appendChild(script);
 })();
