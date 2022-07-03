6028a965af62f11ffefe773356da7deb7a21fddc
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2022-07-03 21:39:14 +0900

    Support dark-reader

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,13 +1,28 @@
+
 /// Invert colors of SVG files.
-(function() {
+function invertSVGColor() {
   var scheme = window.getComputedStyle(document.body).colorScheme;
-  //console.log(scheme);
+  var images = document.getElementsByTagName('img');
   if (scheme == 'dark') {
-    var images = document.getElementsByTagName('img');
     for (var i = 0; i < images.length; i++) {
-      // console.log(images[i]);
       images[i].style.filter += "invert(100%)";
       images[i].style.filter += "grayscale(50%)";
     }
+  } else {
+    for (var i = 0; i < images.length; i++) {
+      images[i].style.filter = null;
+    }
   }
-})();
+}
+
+var colorSchemeObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutationRecord) {
+    invertSVGColor();
+  });
+});
+
+invertSVGColor();
+const target = document.querySelector('html');
+colorSchemeObserver.observe(target,
+  { attributes : true, attributeFilter : ['data-darkreader-scheme'] }
+);
